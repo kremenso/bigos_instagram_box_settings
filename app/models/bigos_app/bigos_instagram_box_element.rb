dc98fe82408074ed614be6fbb78a51f74ef4f8bd
kremenso <kremenso@o2.pl>
2013-01-25 22:53:36 +0100

    init commit

---
+++ app/models/bigos_app/bigos_instagram_box_element.rb
@@ -0,0 +1,9 @@
+module BigosApp
+  class BigosInstagramBoxElement < PageElement
+
+    def self.class_name
+      "Instagram module"
+    end
+
+  end
+end
---
+++ lib/generators/bigos_instagram_box/install_generator.rb
@@ -0,0 +1,24 @@
+module BigosInstagramBox
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../templates", __FILE__)
+
+      desc "Creates a BigosInstagramBox initializer."
+      def copy_initializer
+        template "bigos_instagram_box.rb", "config/initializers/bigos_instagram_box.rb"
+      end
+
+      def create_settings
+      Setting["#{BigosInstagramBox.name}_client_id"] = "YOUR_APP_ACCESS_TOKEN"
+      Setting["#{BigosInstagramBox.name}_client_secret"] = "YOUR_APP_CLIENT_SECRET"
+      Setting["#{BigosInstagramBox.name}_access_token "]= "YOUR_APP_ACCESS_TOKEN"
+      Setting["#{BigosInstagramBox.name}_hashtag"] = ""
+      Setting["#{BigosInstagramBox.name}_user_id"] = ""
+      Setting["#{BigosInstagramBox.name}_user_name"] = ""
+      Setting["#{BigosInstagramBox.name}_latitude"] = ""
+      Setting["#{BigosInstagramBox.name}_longitude"] = ""
+      end
+
+    end
+  end
+end
